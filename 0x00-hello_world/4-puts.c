a2fb295dab19959ec35e040c3bec77fe85c36c1d
N99sammy <sammynjoroge2010@gmail.com>
2022-03-10 10:52:28 -0800

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *   * @void: no arguments
  *    * Description: the program starts and ends here.
  *     * Return: value 0 to exit
- *      */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
