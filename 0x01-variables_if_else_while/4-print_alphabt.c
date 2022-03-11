7d1abb3c23c8941be133ead42b227ac719ddd1ae
N99sammy <sammynjoroge2010@gmail.com>
2022-03-11 13:21:19 -0800

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  *  *main - entry point
+ *   *   *Description: random number in n +ve/-ve
+ *    *    *Return: 0 on success
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if ((ch != 'e') && (ch != 'q'))
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
