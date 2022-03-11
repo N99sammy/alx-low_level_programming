d1ad2a054e9316e6e9cc63daab58502d533047ec
N99sammy <sammynjoroge2010@gmail.com>
2022-03-11 11:01:01 -0800

    yahpu

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  *main - entry point
+ *   *Description: random number in n +ve/-ve
+ *    *Return: 0 on success
+*/
+int main(void)
+{
+       	char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{	
+	putchar( ch);
+}
+return (0);
+}
