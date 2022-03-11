7233cd393a4e2de3df5c0f889365936570b80087
samuel Njoroge <sammynjoroge2010@gmail.com>
2022-03-12 01:19:06 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  *main - entry point
+ *   *Description: random number in n +ve/-ve
+ *    *Return: 0 on success
+*/
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch <= 'a'; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
