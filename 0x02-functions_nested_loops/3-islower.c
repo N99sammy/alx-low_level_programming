47c2b6a75fa4627bfcdfeefdb1d02877c0e31870
N99sammy <sammynjoroge2010@gmail.com>
2022-03-16 14:13:03 -0700

    isotopes 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
