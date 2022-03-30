17a2f51f79cf942419d81a01d01b13d4c32e7c35
N99sammy <sammynjoroge2010@gmail.com>
2022-03-30 13:41:01 -0700

    recursions task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -print strings and newline
+ *
+ * @s: string
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+}
