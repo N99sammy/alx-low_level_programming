f7d28dcf7570e91931128b8aaa1097557c8a8e13
N99sammy <sammynjoroge2010@gmail.com>
2022-04-02 11:43:00 -0700

    print namr of file

---
+++ 0x0A-argc_argv/0_whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc__attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
