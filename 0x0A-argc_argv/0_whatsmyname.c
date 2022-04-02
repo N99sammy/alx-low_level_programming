a9f338792f91b77ef5f00c93ede06ac26173bf23
N99sammy <sammynjoroge2010@gmail.com>
2022-04-02 12:27:47 -0700

    prints a name

--- 0x0A-argc_argv/0_whatsmyname.c
+++ 0x0A-argc_argv/0_whatsmyname.c
@@ -9,7 +9,8 @@
  * Return: 0
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
+
 {
 	printf("%s\n", *argv);
 
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,5 @@
 
 int _putchar(char c);
 int _atoi(char *s);
-typedef int make_iso_compilers_happy
+typedef int make_iso_compilers_happy;
 #endif
