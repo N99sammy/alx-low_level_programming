29474ad925fbf7f1ddfb90865d35187385f3ee25
N99sammy <sammynjoroge2010@gmail.com>
2022-04-05 14:37:20 -0700

    pointer that allocates space in memmory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - copies the string given as a parameter
+ *@str: string to duplicate
+ *
+ * Return: pointer to copied string (success), NULL (Error)
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int x, len;
+
+	if (!str)
+		return (NULL);
+
+	for (len = 0; str[len]; len++)
+		;
+	ptr = (char *)malloc(len * sizeof(char));
+
+	if (!ptr)
+		return (NULL);
+
+	for (x = 0; x <= len; x++)
+		ptr[x] = str[x];
+
+	return (ptr);
+}
