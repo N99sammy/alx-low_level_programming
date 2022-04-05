ed0ed23447166ae4c11550e37e24a81d5e4f8e9b
N99sammy <sammynjoroge2010@gmail.com>
2022-04-05 14:42:49 -0700

    pointer that allocates space in memmory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 
 	for (len = 0; str[len]; len++)
 		;
-	ptr = (char *)malloc(len * sizeof(char)+ 1);
+	ptr = (char *)malloc(len * sizeof(char) + 1);
 
 	if (!ptr)
 		return (NULL);
