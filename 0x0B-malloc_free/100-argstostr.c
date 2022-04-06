ffd6b03504138236359c5e57925f7d1147fa37f1
N99sammy <sammynjoroge2010@gmail.com>
2022-04-05 17:18:00 -0700

    advanced tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,36 +10,29 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *s;
+int i = 0, nc = 0, j = 0, cmpt = 0;
+char *s;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+	return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
+for (; i < ac; i++, nc++)
+	nc += _strlen(av[i]);
 
-	s = malloc(sizeof(char) * (len + 1));
+s = malloc(sizeof(char) * nc + 1);
+if (s == 0)
+	return (NULL);
 
-	if (s == NULL)
-		return (NULL);
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+		s[cmpt] = av[i][j];
 
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			s[k] = av[i][j];
-			k++;
-		}
-		s[k] = '\n';
-		k++;
-	}
-
-	return (s);
+	s[cmpt] = '\n';
+	cmpt++;
 }
+s[cmpt] = '\0';
+
+return (s);
+}
+
