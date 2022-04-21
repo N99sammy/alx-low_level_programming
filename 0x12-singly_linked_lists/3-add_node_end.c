27a386769b182060ba7386b22827feae60819b5e
N99sammy <sammynjoroge2010@gmail.com>
2022-04-21 15:32:20 -0700

    linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,6 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *temp;
 	size_t nchar;
 	new = malloc(sizeof(list_t));
+
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
